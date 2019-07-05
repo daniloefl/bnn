7c27bab29310189e1fdad3b6ffeed859a5556e66
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-07-05 15:19:14 +0200

    Changed data generation to produce more complex system.

--- generate_data.py
+++ generate_data.py
@@ -26,17 +26,24 @@ def main_two_gaussians(filename = 'data.h5'):
 
 def main_rainbow(filename = 'data.h5'):
     # make input file
-    N = 10000
+    N = 9000
     x = {}
 
     f = pd.HDFStore(filename, 'w')
     all_data = np.zeros(shape = (0, 2+2))
     signal = np.zeros( (N, 2) )
-    signal[:,0] = np.random.uniform(low = -10.0, high = 10.0, size = (N,))
-    signal[:,1] = -signal[:,0]**2 + 10.0 + np.random.normal(loc = 0.0, scale = 0.3, size = (N,))
-    bkg = np.zeros( (N, 2) )
-    bkg[:,0] = np.random.uniform(low = -10.0, high = 10.0, size = (N,))
-    bkg[:,1] = -bkg[:,0]**2 + 9.0 + np.random.normal(loc = 0.0, scale = 0.3, size = (N,))
+    signal[:,0] = np.random.uniform(low = -3.0, high = 3.0, size = (N,))
+    signal[:,1] = -signal[:,0]**2 + 5.0 + np.random.normal(loc = 0.0, scale = 0.3, size = (N,))
+    Neff = int(N/3)
+    bkg_1 = np.zeros( (Neff, 2) )
+    bkg_2 = np.zeros( (Neff, 2) )
+    bkg_3 = np.zeros( (Neff, 2) )
+    bkg_1[:,0] = np.random.uniform(low = -3.0, high = 3.0, size = (Neff,))
+    bkg_1[:,1] = -bkg_1[:,0]**2 + 7.5 + np.random.normal(loc = 0.0, scale = 0.4, size = (Neff,))
+    bkg_2[:,0] = np.random.uniform(low = -3.0, high = 3.0, size = (Neff,))
+    bkg_2[:,1] = -bkg_2[:,0]**2 + 2.5 + np.random.normal(loc = 0.0, scale = 0.4, size = (Neff,))
+    bkg_3 = np.random.multivariate_normal(mean = [0, 0], cov = [[1, 0.5],[0.5, 1]], size = (Neff,))
+    bkg = np.concatenate( (bkg_1, bkg_2, bkg_3), axis = 0)
     data   = np.append(signal, bkg, axis = 0)
     data_t = np.append(np.ones(N), np.zeros(N))
     data_w = np.append(np.ones(N), np.ones(N))
